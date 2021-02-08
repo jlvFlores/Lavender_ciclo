25412fbf3a26cf430e7eef4550d73aa62fdc7505
Luis Vazquez <jluisvazquez.jv@gmail.com>
2021-02-08 13:23:02 -0600

    1.5 Creando Formulario

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,35 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
-                
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $errors)
+                                - {{ $error }} <br>
+                                @endforeach
+                            </div>
+                            @endif
+
+                            <form action="{{ route('store') }}" method="POST">
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                        <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name') }}">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
 
                 <table class="tabla">
                     <thead>
