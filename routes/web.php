f99629c3dbdb468736bfcc6a33be989ca20b4fa3
Luis Vazquez <jluisvazquez.jv@gmail.com>
2021-02-08 09:34:01 -0600

    1.4 Que es un crud y como implementarlo

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view ('welcome');
-});
+Route::get('/', 'UserController@index');
+Route::post('users', 'UserController@store')->name('users.store');
+Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+
+
